3d02b04f6bd595cb348645fca4df8de1c9bfbe49
Karthik Banjan <92287364+karthikbanjan@users.noreply.github.com>
2022-06-22 20:27:11 +0530

    Create exchange.py

---
+++ exchange.py
@@ -0,0 +1,33 @@
+import requests as rq
+
+
+def get_exchange_rate(from_currency):
+    url = f'http://www.floatrates.com/daily/{from_currency.lower()}.json'
+    request = rq.get(url).json()
+    return request
+
+
+while True:
+    print()
+    from_currency = input("Enter the currency you want to convert from: ")
+
+    rates = get_exchange_rate(from_currency)
+
+    money = float(input("Enter the amount of money you want to convert: "))
+
+    while True:
+        print()
+        to_currency = input("Enter the currency you want to convert to: ")
+
+        rate = float(rates[to_currency.lower()]["rate"])
+        print("Exchange rate: ", money * rate)
+
+        if input("Do you want to convert to another  currency? (y/n) ") == "y":
+            continue
+        elif input("Do you wish to convert from another currency? (y/n) ") == "y":
+            break
+        else:
+            exit()
+
+
+
